91fac3a3e4e721e09b2e78afadb3bc1ea05f52bd
Ruslan Khaliullin <ruslan_Khaliullin@epam.com>
2021-12-16 16:48:27 +0300

    task 1

--- Assignment_1/Lecture_task_one.py
+++ Assignment_1/Lecture_task_one.py
@@ -1,2 +1,10 @@
 # First task solution
 
+i = 100
+sum = 0
+while i < 200:
+    if i % 3 == 0:
+        print("целое число", i, "кратно 3", sep=" ")
+        sum += i
+    i += 1
+print("Сумма всех целых чисел от 100 до 200 кратные 3 равна", sum, sep=" ")
